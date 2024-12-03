e3cface37137e030ded7349e14f6e06322600a6c
OgnaigO <fery101112@gmail.com>
2024-12-04 00:08:03 +0700

    Lab04

---
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -0,0 +1,77 @@
+package hust.soict.dsai.aims.media;
+
+public class DigitalVideoDisc extends Disc implements Playable{
+
+//	private String director;
+//	private int length;
+
+	
+	private static int nbDigitalVideoDiscs = 0;
+
+	
+	
+	
+	
+//	public DigitalVideoDisc() {
+//		super();
+//		nbDigitalVideoDiscs += 1;
+//		setId(nbDigitalVideoDiscs);
+//	}
+
+	public DigitalVideoDisc(String title) {
+		super(title);
+		nbDigitalVideoDiscs += 1;
+		setId(nbDigitalVideoDiscs);
+	}
+	
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super(title, category, cost);
+		nbDigitalVideoDiscs += 1;
+		setId(nbDigitalVideoDiscs); 
+	}
+	
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super(title, category, director, cost);
+		nbDigitalVideoDiscs += 1;
+		setId(nbDigitalVideoDiscs); 
+
+	}
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super(title, category, director, length , cost);
+		nbDigitalVideoDiscs += 1;
+		setId(nbDigitalVideoDiscs);
+	}
+	
+	public DigitalVideoDisc(int id, String title, String category, String director, int length, float cost) {
+		super(id, title, category, director, length, cost);
+	}
+	
+//	public String toString() {
+//		return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + ": " + this.getCost() + " $" ;
+//	}
+	
+	public boolean isMatch(String title) {
+		String lowercaseTitle = title.toLowerCase();
+		String[] keywords = lowercaseTitle.split(" ");
+		
+		for (String keyword : keywords) {
+			if (this.getTitle().toLowerCase().contains(keyword)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isMatch(int id) {
+		if (this.getId() == id) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void play() {
+		System.out.println("Playing DVD: " + this.getTitle());
+		System.out.println("DVD length: " + this.getLength());
+	}
+}
